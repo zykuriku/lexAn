56c7bb8b9c08b19f561bd61f40e3baab3deccd74
zykuriku <104116273+zykuriku@users.noreply.github.com>
2024-01-15 15:16:21 +0530

    commit 1

---
+++ lexAn/src/main/java/com/kavya/lexAn/LexAnApplication.java
@@ -0,0 +1,13 @@
+package com.kavya.lexAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexAnApplication.class, args);
+	}
+
+}
---
+++ lexAn/src/test/java/com/kavya/lexAn/LexAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavya.lexAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
